0bfe488f1224bb90026ca790b115f8a4071e051b
Andrew Calhoun <heliosprod@me.com>
2015-09-27 16:30:48 -0700

    Updating calhouna's CS290 git repository with two requested files. First Assignment.

---
+++ helloworld.js
@@ -0,0 +1,3 @@
+console.log("Hello World! This is Andrew's first school JS project at OSU.");
+
+alert("Hello! My name is Andrew M. Calhoun. I am a student at OSU and am enrolled in CS290. Go Beavs!");
